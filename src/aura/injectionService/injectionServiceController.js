a3f150ec02a5e57407bfaf096bb6ec7350ffb8fd
Manjot Singh <manjot@manjot-mac.local>
2018-07-19 16:29:28 +0530

    fixed indentation

--- src/aura/injectionService/injectionServiceController.js
+++ src/aura/injectionService/injectionServiceController.js
@@ -1,7 +1,7 @@
 ({
 	getInjectionComponent : function getInjectionComponent(component, event, helper) {
-        var action = component.get('c.getComponentName');
-        action.setBackground();
-        return helper.executeAction(action);
+		var action = component.get('c.getComponentName');
+		action.setBackground();
+		return helper.executeAction(action);
 	}
 })
