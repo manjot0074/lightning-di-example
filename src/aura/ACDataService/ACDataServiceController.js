5a13a93732fa2ff6655b6bad230b6f87dd521451
Manjot Singh <manjot@manjot-mac.local>
2018-07-19 16:13:20 +0530

    fixed indentation

--- src/aura/ACDataService/ACDataServiceController.js
+++ src/aura/ACDataService/ACDataServiceController.js
@@ -1,12 +1,12 @@
 ({
 	getContactsAction : function getContactsAction(component, event, helper) {
 		var params = event.getParam('arguments');
-        if (params && params.payload) {
-            var action = component.get('c.getContacts');
-            action.setParams({
-                account : params.payload.accountId
-            });
-            return helper.executeAction(action);
-        }
+		if (params && params.payload) {
+			var action = component.get('c.getContacts');
+			action.setParams({
+				account : params.payload.accountId
+			});
+		return helper.executeAction(action);
+		}
 	}
 })
